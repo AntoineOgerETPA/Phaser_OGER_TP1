495cd970819a4886e18502baa36e5b54214723b7
AntoineOgerETPA <AntoineOgerETPA@users.noreply.github.com>
2020-02-24 13:10:04 +0100

    ajout sprite

--- monJeu.js
+++ monJeu.js
@@ -39,6 +39,7 @@ function preload(){
 		this.load.image('sol2','assets/platform3.png');
 	this.load.image('bomb','assets/bomb.png');
 	this.load.spritesheet('perso','assets/dudee.png',{frameWidth: 32, frameHeight: 32});
+	this.load.spritesheet('monstre','assets/monstre.png',{frameWidth: 32, frameHeight: 32});
 	this.load.image('life1','assets/vie1.png');
 		this.load.image('life2','assets/vie2.png');
 			this.load.image('life3','assets/vie3.png');
